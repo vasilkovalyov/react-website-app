0fbd797b9956b354741e5793a38cbf8dcf3ee716
Vasiliy <vasiliy.kovalyov@ukad-group.com>
2021-06-12 02:04:41 +0300

    [FEATURE] Created PersonCard and PersonCardSection components

---
+++ client/src/static/common/persons.js
@@ -0,0 +1,42 @@
+export default {
+    Heading: 'Awesome Team',
+    SubHeading: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.',
+    Items: [
+        {
+            Id: 1,
+            Image: {
+                Name: 'Lian Joy',
+                DesktopUrl: 'https://i.postimg.cc/zBZtWbdw/Rectangle-6-copy.jpg',
+                TabletUrl: 'https://i.postimg.cc/zBZtWbdw/Rectangle-6-copy.jpg',
+                MobileUrl: 'https://i.postimg.cc/zBZtWbdw/Rectangle-6-copy.jpg'
+            },
+            Heading: 'Lian Joy',
+            SubHeading: 'CEO/Founder',
+            Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatem sit necessitatibus corrupti, eaque soluta blanditiis laudantium quibusdam!'
+        },
+        {
+            Id: 2,
+            Image: {
+                Name: 'Albert Thanh ',
+                DesktopUrl: 'https://i.postimg.cc/d12W9sXb/Hue-Saturation-1.jpg',
+                TabletUrl: 'https://i.postimg.cc/d12W9sXb/Hue-Saturation-1.jpg',
+                MobileUrl: 'https://i.postimg.cc/d12W9sXb/Hue-Saturation-1.jpg'
+            },
+            Heading: 'Albert Thanh ',
+            SubHeading: 'CEO/Founder',
+            Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatem sit necessitatibus corrupti, eaque soluta blanditiis laudantium quibusdam!'
+        },
+        {
+            Id: 3,
+            Image: {
+                Name: 'Jenn Pereira',
+                DesktopUrl: 'https://i.postimg.cc/LsZQnBV6/Hue-Saturatio1.jpg',
+                TabletUrl: 'https://i.postimg.cc/LsZQnBV6/Hue-Saturatio1.jpg',
+                MobileUrl: 'https://i.postimg.cc/LsZQnBV6/Hue-Saturatio1.jpg'
+            },
+            Heading: 'Jenn Pereira',
+            SubHeading: 'CEO/Founder',
+            Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatem sit necessitatibus corrupti, eaque soluta blanditiis laudantium quibusdam!'
+        }
+    ]
+}
