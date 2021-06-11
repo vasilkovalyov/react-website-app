ac8047169fe76b3359c3170786c748c8b9073782
Vasiliy <vasiliy.kovalyov@ukad-group.com>
2021-06-12 02:04:41 +0300

    [FEATURE] Created PersonCard and PersonCardSection components

--- client/src/views/main/Home.jsx
+++ client/src/views/main/Home.jsx
@@ -1,12 +1,16 @@
 import React, { Fragment } from 'react'
 
 import HeroSectionModel from '../../static/common/hero-section.js'
+import PersonsModel from '../../static/common/persons.js'
+
 import HeroSection from '../../components/public/HeroSection.jsx'
+import PersonCardSection from '../../components/public/PersonCardSection.jsx'
 
 const Home = () => {
     return (
         <Fragment>
             <HeroSection Slides={HeroSectionModel} IsPagination={true} IsArrows={true} />
+            <PersonCardSection {...PersonsModel} />
         </Fragment>
     )
 }
